61e83bf4c92f7488624fa512f2579df523cc87db
Miguel Angel <miguel.anromen@gmail.com>
2024-05-27 18:01:23 -0500

    Styles were added

--- go.mod
+++ go.mod
@@ -5,6 +5,13 @@ go 1.22.3
 require (
 	atomicgo.dev/keyboard v0.2.9 // indirect
 	github.com/containerd/console v1.0.3 // indirect
+	github.com/ebitengine/oto/v3 v3.2.0 // indirect
+	github.com/ebitengine/purego v0.7.1 // indirect
 	github.com/eiannone/keyboard v0.0.0-20220611211555-0d226195f203 // indirect
+	github.com/fatih/color v1.17.0 // indirect
+	github.com/gopxl/beep v1.4.1 // indirect
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.20 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
 	golang.org/x/sys v0.20.0 // indirect
 )
--- pomodore.go
+++ pomodore.go
@@ -4,100 +4,223 @@ import (
 	"fmt"
 	"os"
 	"os/signal"
-	"strings"
 	"syscall"
 	"time"
-  
-  "github.com/eiannone/keyboard"
+
+	"github.com/eiannone/keyboard"
+	"github.com/fatih/color"
+	"github.com/gopxl/beep"
+	"github.com/gopxl/beep/speaker"
+	"github.com/gopxl/beep/wav"
 )
 
 const (
 	defaultWorkPeriod      = 25 * time.Minute
-	defaultBreakPeriod     = 1 * time.Minute
+	defaultBreakPeriod     = 5 * time.Minute
 	defaultLongBreakPeriod = 15 * time.Minute
 )
 
-func main() {
-	if len(os.Args) < 2 {
-		fmt.Println("Usage: pomo <action>")
-		fmt.Println("Actions: work, break, lbreak")
-		return
-	}
-
-	action := strings.ToLower(os.Args[1])
-
-	switch action {
-	case "work":
-		timer(defaultWorkPeriod)
-	case "break":
-		timer(defaultBreakPeriod)
-	case "lbreak":
-		timer(defaultLongBreakPeriod)
-	default:
-		fmt.Printf("%s is not an available action\n", action)
-		fmt.Println("Actions: work, break, lbreak")
-	}
+type TimerType struct {
+	label    string
+	duration time.Duration
+	color    *color.Color
+	endSound TimerEndSound
 }
 
-func timer(duration time.Duration) {
-	remaining := duration
-	ticker := time.NewTicker(time.Second)
-	defer ticker.Stop()
+type TimerStatus int
+
+const (
+	Paused TimerStatus = iota
+	Running
+)
+
+type TimerEndSound int
+
+const (
+	WorkSound TimerEndSound = iota
+	BreakSound
+)
+
+func main() {
+	workTimer := TimerType{
+		label:    "Work",
+		duration: defaultWorkPeriod,
+		color:    color.New(color.FgMagenta),
+		endSound: WorkSound,
+	}
+	breakTimer := TimerType{
+		label:    "Break",
+		duration: defaultBreakPeriod,
+		color:    color.New(color.FgGreen),
+		endSound: BreakSound,
+	}
+	longBreakTimer := TimerType{
+		label:    "Long break",
+		duration: defaultLongBreakPeriod,
+		color:    color.New(color.FgBlue),
+		endSound: BreakSound,
+	}
+
+	drawHeader()
 
 	exitSignal := make(chan os.Signal, 1)
 	signal.Notify(exitSignal, os.Interrupt, syscall.SIGTERM)
 
-	paused := false
-
-  keysEvents, err := keyboard.GetKeys(10)
+	keysEvents, err := keyboard.GetKeys(10)
 	if err != nil {
 		panic(err)
 	}
-	defer func() {
-		_ = keyboard.Close()
-	}()
+	defer keyboard.Close()
 
-	go func() {
-    for {
-      event := <-keysEvents
-      if event.Err != nil {
-        panic(event.Err)
-      }
-      if event.Key == keyboard.KeyCtrlC {
-        killProgram()
-      }
-      if event.Rune == 'p' {
-        paused = !paused
-      }
-    }
-	}()
+	ticker := time.NewTicker(time.Second)
+	defer ticker.Stop()
 
-	for remaining > 0 {
+	var currentTimer *TimerType
+	remaining := 0 * time.Second
+	paused := false
+
+	for {
 		select {
+		case <-ticker.C:
+			if remaining == 0 && currentTimer != nil {
+				if currentTimer.endSound == BreakSound {
+					playWavSound("break-end-sound.wav")
+				} else {
+					playWavSound("end-sound.wav")
+				}
+
+				clearLine()
+				fmt.Printf(">>>>>> %s has finish <<<<<<", currentTimer.label)
+
+				currentTimer = nil
+			}
+			if remaining > 0 {
+				if paused {
+					printTimer(currentTimer, Paused, remaining)
+					continue
+				}
+
+				remaining -= time.Second
+				printTimer(currentTimer, Running, remaining)
+			}
 		case <-exitSignal:
 			clearLine()
-			fmt.Print("Timer cancelled")
+			fmt.Println("The end...")
 			return
-		case <-ticker.C:
-			if !paused {
-				remaining -= time.Second
-				clearLine()
-				fmt.Printf("Remaining %02d:%02d", int(remaining.Minutes()), int(remaining.Seconds())%60)
+		case k := <-keysEvents:
+			if k.Key == keyboard.KeyCtrlC {
+				killProgram()
+			}
+
+			if k.Rune == 'w' && remaining == 0 {
+				currentTimer = &workTimer
+				remaining = workTimer.duration
+			}
+			if k.Rune == 'b' && remaining == 0 {
+				currentTimer = &breakTimer
+				remaining = breakTimer.duration
+			}
+			if k.Rune == 'l' && remaining == 0 {
+				currentTimer = &longBreakTimer
+				remaining = longBreakTimer.duration
+			}
+
+			if k.Rune == 'p' {
+				paused = !paused
 			}
 		}
 	}
+}
+
+func playWavSound(file string) {
+	soundFile, err := os.Open(file)
+	if err != nil {
+		panic(err)
+	}
+
+	streamer, format, err := wav.Decode(soundFile)
+	if err != nil {
+		panic(err)
+	}
+	defer streamer.Close()
+
+	speaker.Init(format.SampleRate, format.SampleRate.N(time.Second/10))
+
+	done := make(chan bool)
+	speaker.Play(beep.Seq(streamer, beep.Callback(func() {
+		done <- true
+	})))
+
+	<-done
+}
+
+func printTimer(timer *TimerType, status TimerStatus, remaining time.Duration) {
+	progress := (float32(timer.duration-remaining) / float32(timer.duration))
+	color := selectTimerColor(timer.color, status)
 
 	clearLine()
-	fmt.Println("\nFinished")
+	color.Print(timer.label, " ")
+	color.Print(getProgressBar(progress), " ")
+	if status == Paused {
+		color.Print("[paused] ")
+	}
+	color.Printf(
+		"%02d:%02d",
+		int(remaining.Minutes()),
+		int(remaining.Seconds())%60,
+	)
+}
+
+func selectTimerColor(timerColor *color.Color, status TimerStatus) *color.Color {
+	if status == Paused {
+		return color.New(color.FgHiRed)
+	}
+	return timerColor
+}
+
+func getProgressBar(progress float32) string {
+	bar := ""
+	barLength := 40
+
+	for i := 0; i < barLength; i++ {
+		if progress >= float32((i+1))/float32(barLength) {
+			bar += "█"
+		} else {
+			bar += "░"
+		}
+	}
+
+	return bar
+}
+
+func drawHeader() {
+	tomatoAsciiArt := `
+============================================================
+
+       """""  """""
+    """     ""     """
+       #####""#####       
+      ##############      Pomodore timer
+      ##############      [w]: Start work timer
+     ################     [b]: Start break timer
+     ################     [l]: Start long break timer
+      #############       [p]: Pause/resume current timer
+       ###########        
+         ######           
+
+============================================================
+  `
+
+	color.Red(tomatoAsciiArt)
 }
 
 func killProgram() {
-  pid := os.Getpid()
-  err := syscall.Kill(pid, syscall.SIGINT)
+	pid := os.Getpid()
+	err := syscall.Kill(pid, syscall.SIGINT)
 
-  if err != nil {
-    panic(err)
-  }
+	if err != nil {
+		panic(err)
+	}
 }
 
 func clearLine() {
